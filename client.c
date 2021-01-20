26cb0f15d45155882826c9272317514b76f4c70b
Sasha Wald <sashawald@gmail.com>
2021-01-20 17:19:22 -0500

    added basic chat functionality

--- client.c
+++ client.c
@@ -7,10 +7,11 @@
 #include <string.h>
 
 int main(){
-    //NOTE: planning to add some simply encryption to the username/password file if there's time. - sasha (if we don't get to it... it would be a good future addition so let that be noted!)
-    //plan for client:
-    //0) ask if user has name. if no, create name/password and add to file. if yes, proceed
+    //NOTE: planning to add duplicate username prevention and some simple encryption to the username/password file if there's time. - sasha (if we don't get to it... it would be a good future addition so let that be noted!)
+
+    //0) ask if user has name. if no, create name/password and add to file. if yes, proceed. if name is in login info file and password is correct, log in. 
     char buffer[256];
+    char finaluser[256];
     
     //asking for existing login
     printf("Do you have an existing login? [y/n]: ");
@@ -29,6 +30,8 @@ int main(){
         write(fd,username,strlen(username));
         write(fd,password,strlen(password));
         close(fd);
+        strcpy(finaluser,username);
+        printf("A new login has been created! You may now type to the chat.\n");
     }
     //user indicated they already have a login
     else if(strcmp(buffer,"y")==0){
@@ -39,28 +42,55 @@ int main(){
         char tempbuff[256];
         read(fd,tempbuff,256);
         int namelen = strlen(username);
+        if(strstr(tempbuff,username) == NULL){
+            printf("Username not found. Please try again.\n");
+            exit(0);
+        }
         //acquiring password from file
-        char temppass[5];
+        char temppass[3];
         for(int i = 2; i < 6; i++){
            temppass[i-2]=(strstr(tempbuff,username)+namelen)[i];
         }
+        //checking password
         printf("Enter the FOUR-NUMBER password associated with your username: ");
         char enteredpass[5];
         scanf("%s",enteredpass);
         if(strcmp(enteredpass,temppass)==0){
-            printf("Correct password entered! Welcome to the chat.\n");
+            printf("Correct password entered! You may now type to the chat.\n");
         }
         else{
-            printf("Incorrect password entered. Please launch chat again.");
+            printf("Incorrect password entered. Please launch chat again.\n");
             exit(0);
         }
+        strcpy(finaluser,username);
     }
     else{
         printf("please pick yes or no.\n");
+        exit(0);
     }
-    //we can add duplicate username protection, but we don't have to
-    //0.5) if name is in login info file and password is correct, log in. 
-    //1) create named pipe for sending messages to server
-    //1.5) messages should be sent with username info
+    //1) send client username to server pipe - then create pipe with username!  
+    int fd1 = open("u_names",O_WRONLY);
+    write(fd1,finaluser,sizeof(finaluser));
+    close(fd1);
+    char textbuffer[256];
+    mkfifo(finaluser,0644);
+    fgets(textbuffer,256,stdin);
+    while(1){
+
+        //message is contained in textbuffer
+        fgets(textbuffer,256,stdin);
+        char finmessg[256];
+        char colon[12]=": ";
+        strcpy(finmessg,finaluser);
+        strcat(finmessg,colon);
+        strcat(finmessg,textbuffer);
+        //strcat(finmessg,textbuffer);
+    
+        int fd2 = open(finaluser,O_WRONLY);
+        write(fd2,finmessg,sizeof(finmessg));
+        close(fd2);
+    }
+
     //2) recieve messages from server which are printed to terminal (accompanied by username that sent message)
+    // not sure if we should do the above! lmk -sasha
 }
