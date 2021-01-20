b6e2fc0ed5021b00182558e736a2917c5d854b54
Sasha Wald <sashawald@gmail.com>
2021-01-20 17:19:48 -0500

    added basic chat functionality

--- server.c
+++ server.c
@@ -7,5 +7,30 @@
 #include <string.h>
 
 int main(){
-    
+    //0) recieve client message info, with username
+    mkfifo("u_names",0644);
+    int i = 0;
+    while(1){
+        int fd1 = open("u_names",O_RDONLY);
+        //supports up to 10 users at once. you can easily change this by adjusting array 'usernames'
+        char usernames[10][256];
+        
+        read(fd1,usernames[i],sizeof(usernames[i]));
+        printf("\n");
+        i++;
+        printf("User %d has joined the chat: %s\n",i,usernames[i-1]);
+        close(fd1);
+
+        //1) send message + username info to all other clients
+        int f = fork();
+        if (f == 0){
+            while(1){
+                int fd2 = open(usernames[i-1],O_RDONLY);
+                char message[256];
+                read(fd2,message,sizeof(message));
+                close(fd2);
+                printf("%s\n",message);
+            }
+        }
+    }
 }
